9f5fb6f53db7cd5b77ae57caf44d23502163b7a1
Elif <elifintizam@gmail.com>
2023-08-15 13:37:40 +0300

    Delete Mapping and Put Mapping

--- src/main/java/com/elifintizam/BillPaymentSystem/MemberAccountController.java
+++ src/main/java/com/elifintizam/BillPaymentSystem/MemberAccountController.java
@@ -17,12 +17,25 @@ public class MemberAccountController {
     }
 
     @GetMapping
-    public List<MemberAccount> getMembers(){
+    public List<MemberAccount> getMembers() {
         return memberAccountService.getMembers();
     }
 
     @PostMapping
-    public void postMember(@RequestBody MemberAccount memberAccount){
+    public void postMember(@RequestBody MemberAccount memberAccount) {
         memberAccountService.postMember(memberAccount);
+    }
+
+    @DeleteMapping(path = "{memberId}")
+    public void deleteMember(@PathVariable("memberId") int memberId) {
+        memberAccountService.deleteMember(memberId);
+    }
+
+    @PutMapping(path = "{memberId}")
+    public void updateMember(@PathVariable("memberId") int memberId,
+                             @RequestParam(required = false) String firstName,
+                             @RequestParam(required = false) String lastName,
+                             @RequestParam(required = false) Double balance) {
+        memberAccountService.updateMember(memberId, firstName, lastName, balance);
     }
 }
--- src/main/java/com/elifintizam/BillPaymentSystem/MemberAccountService.java
+++ src/main/java/com/elifintizam/BillPaymentSystem/MemberAccountService.java
@@ -1,24 +1,56 @@
 package com.elifintizam.BillPaymentSystem;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MemberAccountService {
 
-    private  final MemberAccountRepository memberAccountRepository;
+    private final MemberAccountRepository memberAccountRepository;
 
     @Autowired
-    public MemberAccountService(MemberAccountRepository memberAccountRepository){
+    public MemberAccountService(MemberAccountRepository memberAccountRepository) {
         this.memberAccountRepository = memberAccountRepository;
     }
 
-    public List<MemberAccount> getMembers(){
+    public List<MemberAccount> getMembers() {
         return memberAccountRepository.findAll();
     }
 
     public void postMember(MemberAccount memberAccount) {
         memberAccountRepository.save(memberAccount);
+    }
+
+    public void deleteMember(int memberId) {
+        boolean exists = memberAccountRepository.existsById(memberId);
+        if (!exists) {
+            throw new IllegalStateException("Member with id " + memberId + " does not exist.");
+        }
+        memberAccountRepository.deleteById(memberId);
+    }
+
+    @Transactional
+    public void updateMember(int memberId, String firstName, String lastName, Double balance) {
+        MemberAccount memberAccount = memberAccountRepository.findById(memberId).orElseThrow(() ->
+                new IllegalStateException("Member with id " + memberId + " does not exist."));
+        if(firstName != null &&
+                firstName.length()>0 &&
+                !Objects.equals(memberAccount.getFirstName(),firstName)){
+            memberAccount.setFirstName(firstName);
+        }
+        if(lastName != null &&
+                lastName.length()>0 &&
+                !Objects.equals(memberAccount.getLastName(),lastName)){
+            memberAccount.setLastName(lastName);
+        }
+        if(balance != null &&
+                balance >= 0 &&
+                balance != memberAccount.getBalance()){
+            memberAccount.setBalance(balance);
+        }
     }
 }
