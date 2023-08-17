3fa1f24862b97c58ebd72ccb62bca04885eb5d20
Elif <elifintizam@gmail.com>
2023-08-17 15:42:07 +0300

    Fixes

--- src/main/java/com/elifintizam/BillPaymentSystem/BillRepository.java
+++ src/main/java/com/elifintizam/BillPaymentSystem/BillRepository.java
@@ -1,6 +1,6 @@
 package com.elifintizam.BillPaymentSystem;
 
-import com.elifintizam.BillPaymentSystem.Model.Bill;
+import com.elifintizam.BillPaymentSystem.model.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
